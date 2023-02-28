c5303850f5ecbbb5728c7ea488282be2a14ab836
Albert Cheng <albert@crc-dot1x-nat-10-239-149-129.bu.edu>
2023-02-27 19:21:38 -0500

    DB runs succesfully

--- schema.sql
+++ schema.sql
@@ -1,120 +1,130 @@
 CREATE DATABASE IF NOT EXISTS photoshare;
 USE photoshare;
-DROP TABLE IF EXISTS Pictures CASCADE;
+DROP TABLE IF EXISTS Tagged_With CASCADE;
+DROP TABLE IF EXISTS Tags CASCADE;
+DROP TABLE IF EXISTS Has_Photo CASCADE;
+DROP TABLE IF EXISTS Likes_Photo CASCADE;
+DROP TABLE IF EXISTS Owns_Album CASCADE;
+DROP TABLE IF EXISTS Has_Comment CASCADE;
+DROP TABLE IF EXISTS Photos CASCADE;
+-- DROP TABLE IF EXISTS Pictures CASCADE; -- 
+DROP TABLE IF EXISTS Albums CASCADE;
+DROP TABLE IF EXISTS Commented CASCADE;
+DROP TABLE IF EXISTS Comments CASCADE;
+DROP TABLE IF EXISTS Friends_with CASCADE;
 DROP TABLE IF EXISTS Users CASCADE;
 
 CREATE TABLE Users (
-    First_Name varchar(255),
-    Last_Name varchar(255),   
-    User_id int4  AUTO_INCREMENT,
+	first_name varchar(255),
+    last_name varchar(255),
+    user_id int4  AUTO_INCREMENT,
     email varchar(255) UNIQUE,
-    Dob DATE,
-    Hometown varchar(255),
-    Gender varchar(255),
-    Password varchar(255),
-    CONSTRAINT users_pk PRIMARY KEY (User_id)
+    dob DATE,
+    hometown varchar(255),
+    gender varchar(255),
+    password varchar(255),
+	CONSTRAINT users_pk PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Friends_With(
-    User_1 varchar(255),
-    User_2 varchar(255),
-    PRIMARY KEY (User_1,User_2),
-    FOREIGN KEY (User_1) REFERENCES Users(User_id),
-    FOREIGN KEY (User_2) REFERENCES Users(User_id)
-    -- OR -- 
-    -- CONSTRAINT FRIENDS_PK PRIMARY KEY (User_1,User_2)
-  
+CREATE TABLE Friends_with (
+	user_1 int4,
+    user_2 int4,
+    PRIMARY KEY(User_1, User_2),
+	FOREIGN KEY (user_1) REFERENCES Users(user_id),
+	FOREIGN KEY (user_2) REFERENCES Users(user_id)
+
 );
 
 CREATE TABLE Comments(
-    Comment_id varchar(255) AUTO_INCREMENT,
-    Comment_Text varchar(255),
-    Comment_Create_Date DATE,
-    CONSTRAINT Comment_pk PRIMARY KEY (Comment_id)
+	comment_id int4 AUTO_INCREMENT,
+    comment_text varchar(255),
+    comment_create_date DATE,
+    CONSTRAINT coment_pk PRIMARY KEY (comment_id)
 );
 
 CREATE TABLE Commented (
-    Comment_id varchar(255) AUTO_INCREMENT,
-    User_id varchar(255),
-    PRIMARY KEY (Comment_id),
-    FOREIGN KEY (User_id) REFERENCES Users(User_id),
-    FOREIGN KEY (Comment_id) REFERENCES Comments(Comment_id)
+	comment_id int4,
+    user_id int4,
+    PRIMARY KEY(comment_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id),
+    FOREIGN KEY(comment_id) REFERENCES Comments(comment_id)
 );
-
-CREATE TABLE Has_Comment (
-    Comment_id varchar(255),
-    Photo_id varchar(255),
-    PRIMARY KEY (Comment_id),
-    FOREIGN KEY (Comment_id) REFERENCES Comments(Comment_id),
-	  FOREIGN KEY (Photo_id) REFERENCES Photos(Photo_id)
-
-);
-
-CREATE TABLE Albums(
-  	Album_id varchar(255) AUTO_INCREMENT,
-    Album_Name varchar(255),
-	  album_create_date DATE,
-	  PRIMARY KEY (Album_id)
-
-);
-
-CREATE TABLE Owns_Album (
-	  Album_id varchar(255),
-	  User_id CHAR(10),
-	  PRIMARY KEY(Album_id),
-	  FOREIGN KEY (Album_id) REFERENCES Albums(Album_id),
-	  FOREIGN KEY (User_id) REFERENCES Users(User_id)
+CREATE TABLE Albums (
+	album_name VARCHAR(255),
+    album_id int4 AUTO_INCREMENT,
+    album_create_date DATE,
+    PRIMARY KEY(album_id)
 );
 
 CREATE TABLE Photos(
-    Photo_id varchar(255) AUTO_INCREMENT,
-    Caption varchar(255),
-    Photo_Data longblob,
-    Album_id vharchar(255),
-    PRIMARY KEY (Photo_id),
-    FOREIGN KEY (Album_id) REFERENCES Albums(Album_id)
+	photo_id int4 AUTO_INCREMENT,
+    photo_caption VARCHAR(255),
+    photo_data longblob,
+    album_id int4,
+    PRIMARY KEY(photo_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
+
 
 /*CREATE TABLE Pictures
 (
-  picture_id int4  AUTO_INCREMENT,
-  user_id int4,
-  imgdata longblob ,
-  caption VARCHAR(255),
-  INDEX upid_idx (user_id),
-  CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
+	
+	picture_id int4  AUTO_INCREMENT,
+	user_id int4,
+	imgdata longblob ,
+	caption VARCHAR(255),
+	INDEX upid_idx (user_id),
+	CONSTRAINT pictures_pk PRIMARY KEY (picture_id)
 );
 */
 
+CREATE TABLE Has_Comment (
+	comment_id int4,
+    photo_id int4,
+    PRIMARY KEY(comment_id),
+    FOREIGN KEY(comment_id) REFERENCES Comments(comment_id),
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
+);
+
+
+
+CREATE TABLE Owns_Album (
+	album_id int4,
+    user_id int4,
+    PRIMARY KEY (album_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
 
 CREATE TABLE Likes_Photo (
-    User_id varchar(255),
-	  Photo_id varchar(255),
-    PRIMARY KEY (Photo_id, User_id),
-    FOREIGN KEY (User_id) REFERENCES Users(User_id)
-    FOREIGN KEY (Photo_id) REFERENCES Photos(Photo_id)
+	user_id int4,
+    photo_id int4,
+    PRIMARY KEY (user_id, photo_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id)
 );
 
 CREATE TABLE Has_Photo (
-    Album_id varchar(255), 
-    Photo_id varchar(255), 
-    PRIMARY KEY (Photo_id),
-    FOREIGN KEY (Album_id) REFERENCES Albums(Album_id), 
-    FOREIGN KEY (Photo_id) REFERENCES Photos(Photo_id)
+	album_id int4,
+    photo_id int4,
+    PRIMARY KEY (photo_id),
+    FOREIGN KEY (photo_id) REFERENCES Photos(photo_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
 );
 
 CREATE TABLE Tags (
-	  Tag_Name VARCHAR(255),
-	  PRIMARY KEY (tag_name)
+	tag_name varchar(255),
+    PRIMARY KEY(tag_name)
 );
 
-CREATE TABLE TaggedWith (
-    Photo_id varchar(255), 
-    Tag_Name varchar(255),
-    PRIMARY KEY (Photo_id, Tag_Name),
-    FOREIGN KEY (Photo_id) REFERENCES Photos(Photo_id),
-    FOREIGN KEY (Tag_Name) REFERENCES Tags(Tag_Name)
+CREATE TABLE Tagged_With (
+	photo_id int4,
+    tag_name varchar(255),
+    PRIMARY KEY (photo_id, tag_name),
+    FOREIGN KEY (photo_id) REFERENCES Photos (photo_id),
+    FOREIGN KEY (tag_name) REFERENCES Tags(tag_name)
 );
+
 
 INSERT INTO Users (email, password) VALUES ('test@bu.edu', 'test');
 INSERT INTO Users (email, password) VALUES ('test1@bu.edu', 'test');
